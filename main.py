5e6148a7fc89f3dbf49e4f6d372b06b19838ff07
itought <mountyjapan24@gmail.com>
2025-10-16 13:18:48 +0300

    Audios

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Program
 
 Picrures = (".png", ".jpg", ".jpeg", ".gif")
 Videos = (".mp4", ".mov", ".avi", ".mkv")
+Audio = (".mp3", ".wav", ".aiff")
