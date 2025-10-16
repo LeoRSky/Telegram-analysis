be1beb4756e08ae22345c63956ad4028c4b11f7d
itought <mountyjapan24@gmail.com>
2025-10-16 13:24:43 +0300

    Counter

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from pathlib import Path
 
 path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
 
-Picrures = (".png", ".jpg", ".jpeg", ".gif")
+Pictures = (".png", ".jpg", ".jpeg", ".gif")
 Videos = (".mp4", ".mov", ".avi", ".mkv")
 Audio = (".mp3", ".wav", ".aiff")
 Docs = (".docs", ".docx", ".txt", ".pdf", ".ppt", ".pptx",".rar",".zip")
+
+count_Pictures = count_Videos = count_Audio = count_Docs = 0
+
+
+
