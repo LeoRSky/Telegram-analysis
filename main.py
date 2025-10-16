218a835c12fe356f400126333d638780e22c40f0
itought <mountyjapan24@gmail.com>
2025-10-16 13:31:55 +0300

    Iteration in Pictures

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
+path_telegram = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
 
 Pictures = (".png", ".jpg", ".jpeg", ".gif")
 Videos = (".mp4", ".mov", ".avi", ".mkv")
@@ -9,5 +9,10 @@ Docs = (".docs", ".docx", ".txt", ".pdf", ".ppt", ".pptx",".rar",".zip")
 
 count_Pictures = count_Videos = count_Audio = count_Docs = 0
 
-
+for file in path_telegram.iterdir():
+    if file.is_file():
+        name = file.suffix.lower()
+        if name in Pictures:
+            count_Pictures += 1
+            
 
