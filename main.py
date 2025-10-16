6f844650b1d59055893a03881d551343723148ea
Anastasia011-oss <kononenkosasa19@gmail.com>
2025-10-16 14:24:23 +0300

    GetInfo_Audio

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def GetInfo_Pictures(list_pictures):
     return f"Number of Pictures: {count_pictures}"
 def GetInfo_Videos(list_videos):
     return f"Number of Videos: {count_videos}"
+def GetInfo_Audio(list_audio):
+    return f"Number of Audio: {count_audio}"
 path_telegram = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
 
 Pictures = (".png", ".jpg", ".jpeg", ".gif")
@@ -31,3 +33,4 @@ list_Telegram = "Telegram download analysis"
 print(GetInfo_Telegram(list_Telegram))
 print(GetInfo_Pictures(count_Pictures))
 print(GetInfo_Videos(count_Videos))
+print(GetInfo_Audio(count_Audio))
