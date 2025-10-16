6d316b78deb3e7bed855f754b1c1b26d1efca061
itought <mountyjapan24@gmail.com>
2025-10-16 13:17:17 +0300

    Images

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
 path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
+
+Picrures = (".png", ".jpg", ".jpeg", ".gif")
