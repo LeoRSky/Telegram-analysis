9f53a8cd0ec039b8623df4434b1fc3f4c663e7c3
itought <mountyjapan24@gmail.com>
2025-10-16 13:17:54 +0300

    Videos

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from pathlib import Path
 path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Telegram Desktop\Telegram.lnk")
 
 Picrures = (".png", ".jpg", ".jpeg", ".gif")
+Videos = (".mp4", ".mov", ".avi", ".mkv")
