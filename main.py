c46ff68e310c8b088b448b18113f5c8f533f87cf
itought <mountyjapan24@gmail.com>
2025-10-16 13:20:28 +0300

    Docs

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ path = Path(r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Program
 Picrures = (".png", ".jpg", ".jpeg", ".gif")
 Videos = (".mp4", ".mov", ".avi", ".mkv")
 Audio = (".mp3", ".wav", ".aiff")
+Docs = (".docs", ".docx", ".txt", ".pdf", ".ppt", ".pptx",".rar",".zip")
